1512894025615ebe448c1740fd0a0a701f9775af
Jomatrix54 <spagorosso@gmail.com>
2025-07-20 16:51:53 +0200

    Delete Sources directory

--- Sources/Models/Post.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct Post: Identifiable, Codable {
-  let id: UUID
-  let author: String
-  let title: String
-  let description: String
-  let timestamp: Date
-}
--- Sources/Models/Sources/Models/Reply.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct Reply: Identifiable, Codable {
-  let id: UUID
-  let postId: UUID
-  let author: String
-  let content: String
-  let timestamp: Date
-}
